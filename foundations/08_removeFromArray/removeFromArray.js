02bf1f468f45ca4cc599f8af7405488628fb476f
ipizzle <pizzleforprezident@icloud.com>
2026-07-28 19:32:07 -0700

    removing array chunks

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removal) {
+    let filtered=arr.slice('');
+    removal.forEach(element => {
+        filtered=filtered.filter(item => !(item == element && typeof item == typeof element));
+    });
+        return filtered;
 };
 
 // Do not edit below this line
