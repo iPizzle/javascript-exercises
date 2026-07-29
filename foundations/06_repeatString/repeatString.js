cd07b74398076fb63d29abfcf597a90bd18a3095
ipizzle <pizzleforprezident@icloud.com>
2026-07-28 18:58:16 -0700

    repeated strings

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let repeated='';
+    if (num < 0){
+        return "ERROR"
+    } else 
+        for (let i=1; i<=num; i++){
+            repeated=repeated + str
+        }
+        return repeated;
 };
 
 // Do not edit below this line
